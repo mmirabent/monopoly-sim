2e1608663c43c67489b48ee9cfc45007ee6c1d3e
Marcos Mirabent <mmirabent@gmail.com>
2013-09-05 13:07:37 -0400

    Added csv output

--- sim.rb
+++ sim.rb
@@ -2,9 +2,19 @@ $LOAD_PATH.unshift("#{File.dirname(__FILE__)}/lib") unless $LOAD_PATH.include? "
 
 require 'game'
 require 'logging'
-include Logging
+require 'CSV'
 
 g = Game.new
 for i in 0..100
   g.turn
+end
+
+CSV.open("test.csv", "wb") do |csv|
+  csv << ["Name", "Hits"]
+  
+  g.board.spaces.each do |space|
+    csv << [space.name, space.hit_count]
+  end
+  
+  csv << ["Total hits",100]
 end
